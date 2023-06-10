883b640b69c0af757afbc19f1d8c49117ef3e91e
Hassam Ul Haq <developer.hassam@gmail.com>
2023-06-10 22:04:38 +0500

    Create productsmeta migration

---
+++ database/migrations/2023_06_10_220131_create_productsmeta_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('productsmeta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->string('meta_key', 255)->nullable();
+            $table->text('meta_value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('productsmeta');
+    }
+};
