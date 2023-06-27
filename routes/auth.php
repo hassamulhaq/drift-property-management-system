b7812786f705c3911c052fc8e38c8c7f5bbbdd15
Hassam Ul Haq <developer.hassam@gmail.com>
2023-06-28 00:26:21 +0500

    route add verify-two-factor

--- routes/auth.php
+++ routes/auth.php
@@ -56,4 +56,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+    Route::post('login/verify-two-factor', [\App\Http\Controllers\Auth\TwoFactorVerificationController::class, 'verify'])
+        ->name('two-factor-verification');
 });
